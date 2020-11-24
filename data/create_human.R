b5ef27d3bdee8d0caeb35afa538de7548058185c
Samt <samuel.tuhkanen@gmail.com>
2020-11-24 20:27:52 +0200

    Week 5 Update

--- data/create_human.R
+++ data/create_human.R
@@ -17,21 +17,49 @@ summary(gii)
 
 # rename variables 
 
-colnames(hd) <- c('HDI_Rank', 'Country', 'HDI', 'LifeExp', 'EduExp', 'EduMean', 'GNI', 'GNI-HDI-RANK')
-colnames(gii) <- c('GII.Rank', 'Country', 'GII', 'MatMor', 'AdoBirthRate', 'ParlRep', 'SecEduF', 'SecEduM','LabourF', 'LabourM')
+colnames(hd) <- c('HDI_Rank', 'Country', 'HDI', 'Life.Exp', 'Edu.Exp', 'EduMean', 'GNI', 'GNI-HDI-RANK')
+colnames(gii) <- c('GII.Rank', 'Country', 'GII', 'Mat.Mor', 'Ado.Birth', 'Parli.F', 'SecEduF', 'SecEduM','LabourF', 'LabourM')
 
 
 #new education ratio and labour force participation ratio variables 
 library(dplyr)
 
-gii <- gii %>% mutate(EduRatio = SecEduF/SecEduM)
-gii <- gii %>% mutate(LabRatio = LabourF/LabourM)
+gii <- gii %>% mutate(Edu2.FM = SecEduF/SecEduM)
+gii <- gii %>% mutate(Labo.FM = LabourF/LabourM)
 
 
 #join the datasets together
-library(dplyr)
 
 human <- inner_join(hd, gii, by = c("Country"))
 dim(human) 
 
 write.csv(human, file = "./data/human.csv", row.names=FALSE)
+
+#Week 5 
+
+#change GNI to numeric
+library(stringr)
+
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="")
+human$GNI <- as.numeric(human$GNI)
+
+
+#exclude unused variables (keep the ones below)
+keep <- c('Country', 'Edu2.FM', 'Labo.FM', 'Edu.Exp', 'Life.Exp', 'GNI', 'Mat.Mor', 'Ado.Birth', 'Parli.F')
+human <- select(human, one_of(keep))
+
+#exclude rows with missing values
+human <- filter(human, complete.cases(human))
+
+
+#exclude regions (first 155 rows appear to be countries and the rest are not)
+human <- human[1:155,]
+
+#rownames by country and remove country as variable
+rownames(human) <- human$Country
+human <- select(human, -Country)
+
+#save the data (again)
+write.csv(human, file = "./data/human.csv", row.names=TRUE)
+
+
